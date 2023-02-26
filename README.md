e9ffdc4f94bc0bdafdf29c0066c78432538f70c5
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2023-02-25 20:56:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-garden
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-drgm8k)
---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/notes">Notes</a>
+      <a href="/rss">RSS</a>
+      <a href="/tags">Tags</a>
+      <a href="/donate">Donate</a>
+    </nav>
+    <ContentDoc />
+  </main>
+</template>
